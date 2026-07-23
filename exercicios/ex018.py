30c3df4951d2e2a29134258508a09be063505d4f
Camilaliol <camilaliol@hotmail.com>
2026-07-23 23:12:21 +0100

    Cateto_Oposto_Cateto_Adjacente

--- exercicios/ex018.py
+++ exercicios/ex018.py
@@ -1,6 +1,6 @@
 from math import hypot # uso o from math pois usa a função especifica
 co = float(input("Comprimento do cateto oposto: "))
-ca = float(input("Comprimento do cateto adjaccente :"))
+ca = float(input("Comprimento do cateto adjacente :"))
 h = hypot(co,ca) #h = (co ** 2 + ca ** 2) ** (1/2)
 print(f"A hipotenusa vai medir: {h:.2f} ")
 
