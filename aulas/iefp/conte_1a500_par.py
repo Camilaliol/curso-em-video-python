bae5761d462f44bca68149de325d58e0a1660367
Camila Oliveira <Camilaliol@hotmail.com>
2026-07-28 14:06:18 +0100

    conte_1_500

---
+++ aulas/iefp/conte_1a500_par.py
@@ -0,0 +1,11 @@
+numero = 1
+while  numero <500:
+    p = numero % 2
+    if p  == 1 : # == serve para compara os dois valores sao iguais
+        print(f"{numero} impar")
+    
+    else:
+        print(f"{numero} par")
+    numero += 1     
+
+
