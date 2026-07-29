7754a98e39b43cd2081806215e28725d4f05f66c
Camilaliol <camilaliol@hotmail.com>
2026-07-29 23:08:21 +0100

    exercio_aula

---
+++ aulas.py/aula09.py
@@ -0,0 +1,7 @@
+frase = "Curso em Video Python"
+#print(frase.upper().count("O")) 
+(len(frase.strip())) #o len é o trabanho da frase/ strip tira os espaços
+#print(frase.replace("Python", "Android")) #substitui a palavra Python
+#print(frase.find("Video"))
+dividido = frase.split()
+print(dividido)
