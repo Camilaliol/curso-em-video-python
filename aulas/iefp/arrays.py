db5260146cefd16dc7bcf74fbfd7b1dda24ceec8
Camila Oliveira <Camilaliol@hotmail.com>
2026-07-31 10:21:50 +0100

    arrys

---
+++ aulas/iefp/arrays.py
@@ -0,0 +1,8 @@
+# Cria uma lista com vários nomes.
+# Cada nome possui uma posição, começando pelo índice 0.
+nomes = ["xico", "Pedro", "joao", "ricardo", "juliana", "Magui"]
+
+
+for nome in nomes:
+   
+    print(nome)
